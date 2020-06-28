6ae5b3b9ce7c7842e7d1da4d0dce7a70572c987f
pineapple34 <pineapple6934@gmail.com>
2020-06-28 13:49:03 +0500

    Initial commit

---
+++ AlarmClock/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace AlarmClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CurrentTime = new System.Windows.Forms.Label();
+            this.AlarmTime = new System.Windows.Forms.MaskedTextBox();
+            this.SetAlarm = new System.Windows.Forms.Button();
+            this.StopAlarm = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.TimeToAlarm = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CurrentTime
+            // 
+            this.CurrentTime.AutoSize = true;
+            this.CurrentTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CurrentTime.Location = new System.Drawing.Point(65, 26);
+            this.CurrentTime.Name = "CurrentTime";
+            this.CurrentTime.Size = new System.Drawing.Size(120, 31);
+            this.CurrentTime.TabIndex = 0;
+            this.CurrentTime.Text = "00:00:00";
+            // 
+            // AlarmTime
+            // 
+            this.AlarmTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AlarmTime.Location = new System.Drawing.Point(94, 72);
+            this.AlarmTime.Mask = "00:00";
+            this.AlarmTime.Name = "AlarmTime";
+            this.AlarmTime.Size = new System.Drawing.Size(60, 30);
+            this.AlarmTime.TabIndex = 1;
+            this.AlarmTime.ValidatingType = typeof(System.DateTime);
+            // 
+            // SetAlarm
+            // 
+            this.SetAlarm.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SetAlarm.Location = new System.Drawing.Point(42, 155);
+            this.SetAlarm.Name = "SetAlarm";
+            this.SetAlarm.Size = new System.Drawing.Size(171, 29);
+            this.SetAlarm.TabIndex = 2;
+            this.SetAlarm.Text = "Установить будильник";
+            this.SetAlarm.UseVisualStyleBackColor = true;
+            this.SetAlarm.Click += new System.EventHandler(this.SetAlarm_Click);
+            // 
+            // StopAlarm
+            // 
+            this.StopAlarm.Enabled = false;
+            this.StopAlarm.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StopAlarm.Location = new System.Drawing.Point(42, 201);
+            this.StopAlarm.Name = "StopAlarm";
+            this.StopAlarm.Size = new System.Drawing.Size(171, 38);
+            this.StopAlarm.TabIndex = 3;
+            this.StopAlarm.Text = "Остановить будильник";
+            this.StopAlarm.UseVisualStyleBackColor = true;
+            this.StopAlarm.Click += new System.EventHandler(this.StopAlarm_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // TimeToAlarm
+            // 
+            this.TimeToAlarm.AutoSize = true;
+            this.TimeToAlarm.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TimeToAlarm.Location = new System.Drawing.Point(39, 114);
+            this.TimeToAlarm.Name = "TimeToAlarm";
+            this.TimeToAlarm.Size = new System.Drawing.Size(0, 17);
+            this.TimeToAlarm.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 251);
+            this.Controls.Add(this.TimeToAlarm);
+            this.Controls.Add(this.StopAlarm);
+            this.Controls.Add(this.SetAlarm);
+            this.Controls.Add(this.AlarmTime);
+            this.Controls.Add(this.CurrentTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Будильник";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CurrentTime;
+        private System.Windows.Forms.MaskedTextBox AlarmTime;
+        private System.Windows.Forms.Button SetAlarm;
+        private System.Windows.Forms.Button StopAlarm;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label TimeToAlarm;
+    }
+}
+
---
+++ AlarmClock/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace AlarmClock
+{
+    public partial class Form1 : Form
+    {
+        public bool AlarmSet = false;
+        public string[] time;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            CurrentTime.Text = DateTime.Now.TimeOfDay.ToString().Substring(0, 8);
+
+            if (AlarmSet)
+            {
+                TimeToAlarm.Text = "До сигнала будильника: \n" + 
+                    (int.Parse(time[0]) - DateTime.Now.Hour).ToString() + " часов " + Math.Abs(int.Parse(time[1]) - DateTime.Now.Minute).ToString() + " минут(а)";
+            }
+            if (AlarmSet && CurrentTime.Text.Substring(0,5) == AlarmTime.Text)
+            {
+                Alarm();
+            }
+        }
+
+        private void SetAlarm_Click(object sender, EventArgs e)
+        {
+            if (AlarmSet)
+            {
+                AlarmSet = false;
+                SetAlarm.Text = "Установить будильник";
+                TimeToAlarm.Text = "";
+                StopAlarm.Enabled = false;
+            }
+            else
+            {
+                time = AlarmTime.Text.Split(':');
+                if (!int.TryParse(time[0], out int hour) || !int.TryParse(time[1], out int minute) || hour > 23 || minute > 59)
+                {
+                    MessageBox.Show("Время введено неверно");
+                }
+                else
+                {
+                    AlarmSet = true;
+                    SetAlarm.Text = "Удалить будильник";
+                    StopAlarm.Enabled = true;
+                }
+            }
+        }
+
+        private void StopAlarm_Click(object sender, EventArgs e)
+        {
+            AlarmSet = false;
+            StopAlarm.Enabled = false;
+            AlarmTime.Clear();
+            SetAlarm.Text = "Установить будильник";
+            TimeToAlarm.Text = "";
+        }
+
+        public void Alarm()
+        {
+            SoundPlayer sp = new SoundPlayer();
+            sp.SoundLocation = "sound.wav";
+            sp.Play();
+        }
+    }
+}
